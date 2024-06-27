c935907d44db9d350b239f00fb99b3be6baf879b
Jatin-2005 <119180793+Jatin-2005@users.noreply.github.com>
2024-06-27 19:06:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+#define ll long long int
+#define LD long double
+
+signed main()
+{
+    //freopen("IN", "r", stdin);
+    //freopen("OUT", "w", stdout);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    // pre();
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+    	solve();
+    }
+    return 0;
+}
